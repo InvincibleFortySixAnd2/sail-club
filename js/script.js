9e83972ea1cf85ae881f1e9bd182f097164ccb09
FortySixAnd2 <jmaxj91j@gmail.com>
2025-06-15 16:05:32 +0300

    feat: Add javascript.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Theme
+
 const themeButtonLight = document.querySelector('.theme-button-light');
 const themeButtonDark = document.querySelector('.theme-button-dark');
 
@@ -13,6 +15,7 @@ themeButtonDark.addEventListener('click', () => {
   themeButtonDark.classList.add('active');
 });
 
+// Fonts
 
 const fontButtonSansSerif = document.querySelector('.font-button-sans-serif');
 const fontButtonSerif = document.querySelector('.font-button-serif');
@@ -29,3 +32,52 @@ fontButtonSerif.addEventListener('click', () => {
   fontButtonSerif.classList.add('active');
 });
 
+
+// Articles
+
+const blogArticles = document.querySelectorAll('.blog-article');
+for (const blogArticle of blogArticles) {
+  const moreButton = blogArticle.querySelector('.more');
+
+  moreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    blogArticle.classList.remove('short');
+  });
+};
+
+
+// Cards layout
+
+const cardViewButtonGrid = document.querySelector('.card-view-button-grid');
+const cardViewButtonList = document.querySelector('.card-view-button-list');
+const cardsList = document.querySelector('.cards');
+
+cardViewButtonGrid.addEventListener('click', () => {
+  cardsList.classList.remove('list');
+  cardViewButtonGrid.classList.add('active');
+  cardViewButtonList.classList.remove('active');
+});
+
+cardViewButtonList.addEventListener('click', () => {
+  cardsList.classList.add('list');
+  cardViewButtonGrid.classList.remove('active');
+  cardViewButtonList.classList.add('active');
+});
+
+
+// Gallery
+
+let activePhoto = document.querySelector('.active-photo');
+let previews = document.querySelectorAll('.preview-list li a');
+
+for (let preview of previews) {
+  preview.onclick = (evt) => {
+    evt.preventDefault();
+    activePhoto.src = preview.href;
+  
+    let currentActivePreview = document.querySelector('.preview-list li .active-item');
+    currentActivePreview.classList.remove('active-item');
+    preview.classList.add('active-item');
+  };
+}
